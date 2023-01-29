c8a7bdded5affb907a796ef49d0b6d4d88d3a410
hitak77 <hitak77@naver.com>
2023-01-30 05:24:35 +0900

    level0 aya ye woo ma

---
+++ src/main/kotlin/solutionAYA.kt
@@ -0,0 +1,11 @@
+class solutionAYA {
+    fun solution(babbling: Array<String>): Int {
+        val count: Int
+        count = babbling.count {
+            it.isNotEmpty() && it.split("aya", "ye", "woo", "ma").none{
+                it.isNotEmpty()
+            }
+        }
+        return count
+    }
+}
